a0900f9aa055791a7ea8427dadf042e93e1f658d
gregdp <gregdp@gmail.com>
2025-10-29 15:08:30 -0700

    updates res_q

--- src/res_q.py
+++ src/res_q.py
@@ -4,10 +4,10 @@
 toFileName = "/Users/greg/Desktop/Residue_Qscores"
 
 r = 2.9
-mean=-0.0016*r*r*r+0.0434*r*r-0.3956*r+1.3366
-peak=mean+0.024
-low=mean-0.126
-high=mean+0.109
+mean = -0.0016*r*r*r+0.0434*r*r-0.3956*r+1.3366
+peak = mean+0.024
+low = mean-0.126
+high = mean+0.109
 
 print("Q - peak:%.2f, low:%.2f, high:%.2f"%(peak,low,high))
 
